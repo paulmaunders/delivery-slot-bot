cf3156f63663f9fd4779037c0d2c6adb924b5640
Paul Maunders <paulmaunders@pauls-air.lan>
2020-03-28 14:03:03 +0000

    Add support for Tesco.net delivery slots and pushover notifications

---
+++ delivery-slots.js
@@ -0,0 +1,108 @@
+const puppeteer = require("puppeteer");
+const ini = require("ini");
+var fs = require("fs");
+var Push = require("pushover-notifications");
+
+// Read config
+const config = ini.parse(fs.readFileSync("./config.ini", "utf-8"));
+
+// Log time
+var executiontime = Date.now();
+console.log(executiontime);
+
+// Create screenshot folder if it doesn't exist
+var dir = config.output_dir + "/" + executiontime;
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
+
+// Check for delivery slot
+
+async function run() {
+  const browser = await puppeteer.launch();
+
+  try {
+    // TESCO
+
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1366, height: 768 });
+
+    // Login
+    await page.goto("https://secure.tesco.com/account/en-GB/login?from=/");
+    await page.type("#username", config.tesco_username);
+    await page.type("#password", config.tesco_password);
+    await page.screenshot({ path: dir + "/tesco-login.png" });
+    await page.click("#sign-in-form > button");
+    await page.screenshot({ path: dir + "/tesco-logged-in.png" });
+    await page.goto("https://www.tesco.com/groceries/en-GB/slots/delivery");
+
+    var html = await page.content();
+
+    // Look for delivery pages
+    var deliveryDates = await page.$$eval(
+      ".slot-selector--week-tabheader-link",
+      elements =>
+        elements.map(item => {
+          return (itemarray = { date: item.textContent, url: item.href });
+        })
+    );
+
+    // console.log(deliveryDates);
+
+    // Loop through delivery pages and check if slots are available
+    for (var i = 0, len = deliveryDates.length; i < len; i++) {
+      item = deliveryDates[i];
+      console.log("Opening " + item.url + " [" + item.date + "]");
+      await page.goto(item.url);
+      console.log("Taking screenshot");
+      screenshotPath = dir + "/tesco-delivery" + i + ".png"
+      await page.screenshot({
+        path: screenshotPath,
+        fullPage: true
+      });
+      //if (i<2) {
+      if (html.includes("No slots available! Try another day")) {
+        console.log("No slots");
+      } else {
+        console.log("SLOTS AVAILABLE!!!");
+
+        // Send push notification
+
+        var p = new Push({
+          token: config.pushover_api_token
+        });
+
+        var msg = {
+          message: "Delivery slots available between " + item.date,
+          title: "Delivery Slot Bot",
+          sound: "magic", // optional
+          priority: 1, // optional,
+          file: screenshotPath // optional
+          // see test/test_img.js for more examples of attaching images
+        };
+
+        for (var i = 0, l = config.pushover_notification_users.length; i < l; i++) {
+          msg.user = config.pushover_notification_users[i];
+          // token can be overwritten as well.
+
+          p.send(msg, function(err, result) {
+            if (err) {
+              throw err;
+            }
+
+            console.log(result);
+          });
+        }
+
+
+      }
+    }
+  } catch (err) {
+    console.error(err.message);
+  } finally {
+    await browser.close();
+  }
+}
+
+run();
