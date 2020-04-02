45ae6ceaaeb3b7bccd3a55267f5db30a8ff44d0c
Paul Maunders <paul@maunders.net>
2020-04-02 19:16:48 +0100

    Prettified script

--- test-push.js
+++ test-push.js
@@ -10,26 +10,25 @@ const yargs = require("yargs");
 // Read config
 const config = ini.parse(fs.readFileSync("./config.ini", "utf-8"));
 
-
 // Send push notification
 
 const p = new Push({
-    token: config.pushover_api_token,
+  token: config.pushover_api_token,
+});
+
+const msg = {
+  message: "Test push notification ",
+  title: "Delivery Slot Bot",
+  sound: "magic", // optional
+  priority: 1,
+};
+
+for (const user of config.pushover_notification_users) {
+  p.send({ ...msg, user }, function (err, result) {
+    if (err) {
+      throw err;
+    }
+
+    console.log(result);
   });
-
-  const msg = {
-    message: "Test push notification ",
-    title: "Delivery Slot Bot",
-    sound: "magic", // optional
-    priority: 1
-  };
-
-  for (const user of config.pushover_notification_users) {
-    p.send({ ...msg, user }, function (err, result) {
-      if (err) {
-        throw err;
-      }
-
-      console.log(result);
-    });
-  }
+}
